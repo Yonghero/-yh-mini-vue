7ca5db7bf616006877651bf263edd6288c517ced
younghero <yxzhang@hitotek.com>
2022-04-06 22:56:27 +0800

    feat: scheduler & effect runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -2,13 +2,13 @@ class ReactiveEffect {
 
   private _fn: Function
 
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
 
   run(){
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -39,14 +39,20 @@ export function trigger(target, key) {
   const deps = depsMap.get(key)
 
   for (const dep of deps) {
-    dep.run()
+    if(dep.scheduler) {
+      dep.scheduler()
+    }else {
+      dep.run()
+    }
   }
 }
 
 
-export function effect(fn) {
+export function effect(fn, options:any = {}) {
 
-  const _effect = new ReactiveEffect(fn)
+  const _effect = new ReactiveEffect(fn, options.scheduler)
 
   _effect.run()
+
+  return _effect.run.bind(_effect)
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -18,4 +18,54 @@ describe("effect", () => {
     user.age++;
     expect(nextAge).toBe(12);
   });
+
+
+  it('run effect return runner', () => {
+    let foo = 10
+
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+
+    const r = runner()
+
+    expect(foo).toBe(12)
+
+    expect(r).toBe('foo')
+
+  })
+
+  it('scheduler', () => {
+
+    const obj = reactive({ foo: 1 })
+    let dummy
+    let run: any
+
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+
+    const runner = effect(
+      () => {
+       dummy = obj.foo
+      },
+      { scheduler }
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+
+    obj.foo++
+
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(dummy).toBe(1)
+
+    run()
+
+    expect(dummy).toBe(2)
+
+  })
 });
