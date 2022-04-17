5b7b633f312d82a4aa2aceefbbba215fabf0e34b
younghero <yxzhang@hitotek.com>
2022-04-17 12:23:46 +0800

    chore: merge

--- src/reactivity/tests/reactive.spec.ts
+++ src/reactivity/tests/reactive.spec.ts
@@ -1,7 +1,7 @@
-import { isReactive, isReadonly, reactive, readonly }  from '../reactive'
+import { isReactive, isReadonly, reactive, readonly, isProxy }  from '../reactive'
 describe("reactive", () => {
   it("happy path", () => {
-    const original = { foo: 1, hhh:{ a:1 } };
+    const original = { foo: 1 };
     const observed = reactive(original);
     expect(observed).not.toBe(original);
     expect(observed.foo).toBe(1);
@@ -9,16 +9,12 @@ describe("reactive", () => {
     expect(isReactive(observed)).toBe(true)
     expect(isReactive(original)).toBe(false)
 
-    expect(isReactive(observed.hhh)).toBe(true)
-    expect(isReactive(original.hhh)).toBe(false)
-
-
     const readonlyObj = readonly(original)
     expect(isReadonly(observed)).toBe(false)
     expect(isReadonly(original)).toBe(false)
     expect(isReadonly(readonlyObj)).toBe(true)
 
-    expect(isReadonly(readonlyObj.hhh)).toBe(true)
-    expect(isReadonly(original.hhh)).toBe(false)
+    expect(isProxy(observed)).toBe(true)
+    expect(isProxy(readonlyObj)).toBe(true)
   });
 });
